4244566858382b96e8cac24d94fb933a844ddff9
JoeMcCarthy22 <Joe_McCarthy22@hotmail.co.uk>
2026-07-24 16:18:39 +0100

    added task query and tasks query with filtering and pagination

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -12,5 +12,51 @@ builder.queryType({
             });
         }
     }),
+    
+    tasks: t.field({
+        type: ["Task"],
+        args: {
+            taskListId: t.arg.int({
+                required: true,
+            }),
+            completed: t.arg.boolean({
+                required: false,
+            }),
+            skip: t.arg.int({
+                required: false,
+            }),
+            take: t.arg.int({
+                required: false,
+            }),
+        },
+        resolve: async(_parent, args, ctx) => { 
+            return ctx.db.task.findMany({
+                where: {
+                    taskListId: args.taskListId,
+                    completed: args.completed,
+                },
+                orderBy: {
+                    createdAt: "asc",
+                },
+                skip: args.skip,
+                take: args.take,
+            })}
+    }),
+
+    task: t.field({
+        type: "Task",
+        args: {
+            id: t.arg.int({
+                required: true,
+            }),
+        },
+        resolve: async(_parent, args, ctx) => {
+            return ctx.db.task.findUnique({
+                where: {
+                    id: args.id,
+                },
+            });
+        }
+    })
   }),
 });
